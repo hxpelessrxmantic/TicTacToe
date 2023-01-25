66befcff16518850ef606263789c7b4cfc7e8c1d
hxpelessrxmantic <hxpelessrxmantic301@gmail.com>
2023-01-25 13:46:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 var cells = document.querySelectorAll('.cell');
 var turn = 0;
 var grid_values = ["","","","","","","","",""];
+var values = [true,true,true,true,true,true,true,true,true];
 let isGameActive = true;
 
 cells.forEach(cell => {
     cell.addEventListener('click' ,() => {
-        if(turn ===0)
+        var index=cell.getAttribute("id");
+        console.log(values[index]);
+
+        if(turn ===0 && values[index])
+        {
         cell.style.backgroundColor="red";
-        else
-        cell.style.backgroundColor="blue"
-        turn = 1-turn
+        values[index]= false;
+        grid_values[index]=0;
+        console.log(grid_values[index]);
+        turn = 1-turn;
+        }
+        else if (values[index])
+        {
+        cell.style.backgroundColor="blue";
+        values[index]=false;
+        grid_values[index]=1;
+        console.log(grid_values[index]);
+        turn = 1-turn;
+        }
+        // console.log(cell);
+    else{
+        
+        }
+        
     })
 });
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    grid_values[clickedCellIndex] = turn;
-    clickedCell.innerHTML = turn;
-}
-
-function handleCellClick(clickedCellEvent) {
-    const clickedCell = clickedCellEvent.target;
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('id'));
-
-    if (grid_values[clickedCellIndex] !== "" || !isGameActive) {
-        return;
-    }
-
-    handleCellPlayed(clickedCell, clickedCellIndex);
-}
 
